af8d8805461ca865f758fa221d07ecfef3948150
Kinzarka <perminovpetr@gmail.com>
2023-10-15 23:04:18 -0700

    Add 2 tests

---
+++ lesson1/test_add_to_cart_catalog.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Firefox()
+
+
+def test_add_to_cart_catalog():
+    driver.get("https://www.saucedemo.com")
+    user_name = driver.find_element(By.CSS_SELECTOR, 'input[data-test="username"]')
+    user_name.send_keys("standard_user")
+    password_input = driver.find_element(By.CSS_SELECTOR, 'input[data-test="password"]')
+    password_input.send_keys("secret_sauce")
+    log_button = driver.find_element(By.CSS_SELECTOR, 'input[data-test="login-button"]')
+    log_button.click()
+    backpack_add_to_cart_button = driver.find_element(By.CSS_SELECTOR, "button[data-test='add-to-cart-sauce-labs-backpack']")
+    backpack_add_to_cart_button.click()
+    shopping_cart_badge = driver.find_element(By.CSS_SELECTOR,".shopping_cart_badge")
+    assert shopping_cart_badge.is_displayed()
+    driver.quit()
+
+
+
+
---
+++ lesson1/test_failed_auth.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+(from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+driver = webdriver.Firefox()
+
+def test_login_form():
+    driver.get("https://www.saucedemo.com/")
+    error_text = "Epic sadface: Sorry, this user has been locked out."
+
+
+    user_name = driver.find_element(By.CSS_SELECTOR, 'input[data-test="username"]')
+    user_name.send_keys("user")
+    password_input = driver.find_element(By.CSS_SELECTOR, 'input[data-test="password"]')
+    password_input.send_keys("user")
+    login_button = driver.find_element(By.CSS_SELECTOR, 'input[data-test="login-button"]')
+    login_button.click()
+    try:
+        error_message = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//*[text()[contains(.,'Epic sadface: Username and password do not match any user in this service')]]")))
+        assert error_text in error_message.text
+        print("Assertion: Text {} is present on the page.".format(error_text))
+    except AssertionError:
+        print("Assertion: Text {} is not present on the page.".format(error_text))
+    except Exception as e:
+        print("An error occurred: {}".format(str(e)))
+
+    driver.quit()
